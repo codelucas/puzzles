9fa58ed0676cebe0f65e144ee0c228b204840d5c
Lucas Ou-Yang <lucasyangpersonal@gmail.com>
2014-10-12 13:15:28 -0700

    Add new skeleton .c interview questions, remove stupid old solutions

---
+++ c_interview/hanoi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+ * You have 3 towers and N disks of diff sizes.
+ * Start with disks sorted in ascending order on one stack.
+ * 1.) One disk can move at a time.
+ * 2.) A disk is slipped off top of one tower to next.
+ * 3.) Disks can only be placed on larger disks.
+ *
+ * Write a program to move the disks from one tower to another.
+ */
+
+void hanoi()
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
---
+++ c_interview/steps.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * A child is running up a staircase with n steps. He can go 1, 2, or 3
+ * steps at a time. Count how many possible ways he can run up the stairs.
+ * 
+ * Solve both naively and with DP
+ */
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
